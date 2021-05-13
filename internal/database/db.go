edf72fae5a5587c2c212eedb8d648b6c47e7b306
TacitR <48092314+TacitR@users.noreply.github.com>
2021-05-13 09:08:00 +0800

    Update db.go

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"os"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -10,8 +11,16 @@ import (
 var DB *gorm.DB
 
 func connect() (err error) {
-	dsn := "host=tacitr.com user=proxypool password=proxypool dbname=proxypool port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent),})
+	dsn := "user=proxypool password=proxypool dbname=proxypool port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	if url := config.Config.DatabaseUrl; url != "" {
+		s3:=dsn+"host="+url
+	}
+	if url := os.Getenv("DATABASE_URL"); url != "" {
+		s3:=dsn+"host="+url
+	}
+	DB, err = gorm.Open(postgres.Open(s3), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err == nil {
 		fmt.Println("DB connect success: ", DB.Name())
 	}
